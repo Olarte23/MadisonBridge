4da2627def35f9fefbbfdac942eb0b421bcd4eb8
Olarte23 <cristianolarte321414@correo.itm.edu.co>
2023-03-16 16:14:12 -0500

    Add project files.

---
+++ Bridge.Logic/Bridges.cs
@@ -0,0 +1,52 @@
+﻿namespace Bridge.Logic
+{
+    public class Bridges
+    {
+        private readonly string _bridge;
+
+        public Bridges(String bridge)
+        {
+            _bridge = bridge;
+        }
+        public String ValidateBridge()
+        {
+            int countPlatform = 0;
+
+            if (!(_bridge[0] == '*' && _bridge[_bridge.Length-1] == '*'))
+            {
+                return "Invalide Bridge";
+            }
+            for (int i = 1; i < _bridge.Length - 1; i++)
+            {
+                if (!(_bridge[i].Equals('=') || _bridge[i].Equals('+')))
+                {
+                    return "La viga esta mal construida!";
+                }
+            }
+            for (int i = 1; i < _bridge.Length-1; i++)
+            {
+                if (!(_bridge[i].Equals(_bridge[_bridge.Length - i - 1])))
+                {
+                    return "La viga esta mal construida!";
+                }
+            }
+            for (int i = 1; i < _bridge.Length - 1; i++)
+            {
+                if (_bridge[i].Equals('='))
+                {
+                    countPlatform++;
+                    if (countPlatform > 2)
+                    {
+                        return "La viga esta mal construida, tiene mas plataformas!";
+                    }
+                }
+                else
+                {
+                    countPlatform = 0;
+                }  
+            }
+            return "Valid";
+            
+        }
+    }
+}
---
+++ Console/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using Bridge.Logic;
+using System.Linq.Expressions;
+
+var bridge = new Bridges[]
+{
+    new Bridges("*===+==+==*"),
+    new Bridges("*==+==+==*")
+
+};
+
+foreach (var item in bridge)
+{
+    Console.WriteLine(item.ValidateBridge());
+}
